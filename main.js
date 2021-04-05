4b0239a4d70671d42ad369aae8d52d539fd0fea6
Goback9 <75929653+pjch8908@users.noreply.github.com>
2021-04-05 15:37:29 +0900

    toggle

--- main.js
+++ main.js
@@ -1,29 +1,13 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  multiple: function (a, b) {
-    return a * b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  remainder: function (a, b) {
-    return a % b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+const title = document.querySelector("#title");
 
-const plus = calculator.plus(5, 5);
-const minus = calculator.minus(5, 5);
-const multiple = calculator.multiple(5, 5);
-const divide = calculator.divide(5, 5);
-const remainder = calculator.remainder(6, 4);
-const power = calculator.power(2, 3);
+const CLICKED_CLASS = "clicked";
 
-console.log(power);
+function handleClick() {
+  title.classList.toggle(CLICKED_CLASS);
+}
+
+function init() {
+  title.addEventListener("click", handleClick);
+}
+
+init();
