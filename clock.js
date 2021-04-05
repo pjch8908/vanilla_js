3afa2e64c8e2736a20631e13c020efca24756a38
Goback9 <75929653+pjch8908@users.noreply.github.com>
2021-04-05 16:30:02 +0900

    Making a JS Clock Part Two(setInterval)

--- clock.js
+++ clock.js
@@ -6,11 +6,16 @@ function getTime() {
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
+// (조건) ? (true) : (false)
 
 function init() {
   getTime();
+  setInterval(getTime, 1000);
+  // setInterval(함수, (간격)millisecond)
 }
 
 init();
