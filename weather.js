65d4a154254302c1c07d25f5d64f6631d747606e
Goback9 <75929653+pjch8908@users.noreply.github.com>
2021-04-20 18:31:01 +0900

    Getting the Weather part

--- weather.js
+++ weather.js
@@ -1,5 +1,23 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "33bf64520ce50a9d40dfddcbaf764f39";
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+      //  json / JSON임
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `온도 ${temperature} @
+       장소 ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +31,7 @@ function handleGeoSucess(positon) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -24,11 +43,12 @@ function askForCoords() {
 }
 
 function loadCoords() {
-  const loadedCords = localStorage.getItem(COORDS);
-  if (loadedCords === null) {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
     askForCoords();
   } else {
-    //getweather
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 
